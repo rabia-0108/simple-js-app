629d3ae0eee2d5faf4346401f30cef4f15c698b9
rabia-0108 <rabia.esya@gmail.com>
2022-01-02 08:13:44 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert('Hello world');
+
+let age = 20;
+
+let myName = 'Rabia';
+document.write(myName);
+myName = 'Rabia Turkyilmaz';
+document.write(myName);
+
+let title = 'this is the title';
+
+let title2 = 'Hello!';
+
+let size = 20;
+let doubleSize = 20 * 2;
+let minSize = (doubleSize * 2) - (size/2);
+document.write(minSize);
