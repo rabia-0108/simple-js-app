788f1fa97db642992d66bdeda7b43ee9c31a633a
rabia-0108 <rabia.esya@gmail.com>
2022-01-05 17:41:34 +0200

    pokedex list
    after picking some characters from pokedex.org I made the pokemonList as it was instructed.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let pokemonList = [
+  {
+    name:'Squirtle',
+    type: ['water'],
+    height: 50.8,
+    pokedexNumber:'#07'
+    
+  };
+  {
+    name:'Slowbro',
+    type:['water', 'psychic'],
+    height: 152.5,
+    pokedexNumber:'#80'
+
+  },
+  
+  {
+  name: 'Roselia',
+  type: ['grass', 'poison'],
+  height: 30.48,
+  pokedexNumber:'#315'
+}
+];
