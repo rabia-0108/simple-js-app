b987b9b6aca08cd02bca5bc5ee041ad23d828d6c
rabia-0108 <rabia.esya@gmail.com>
2022-01-07 15:53:53 +0200

    for loop
    added some for loops, also using the if conditional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 let pokemonList = [
-  {
-    name:'Squirtle',
-    type: ['water'],
-    height: 50.8,
-    pokedexNumber:'#07'
-    
-  };
-  {
-    name:'Slowbro',
-    type:['water', 'psychic'],
-    height: 152.5,
-    pokedexNumber:'#80'
-
-  },
-  
-  {
-  name: 'Roselia',
-  type: ['grass', 'poison'],
-  height: 30.48,
-  pokedexNumber:'#315'
-}
+  {  name:'Squirtle', type: ['water'], height: 50.8, pokedexNumber:'#07' };
+  { name:'Slowbro', type:['water', 'psychic'], height: 152.5, pokedexNumber:'#80' },
+  { name: 'Roselia', type: ['grass', 'poison'], height: 30.48, pokedexNumber:'#315'}
 ];
+
+for (let i = 0; i < pokemonList.length; i++ ) {
+  document.write('<br>' + pokemonList[i].name + (' (height)') + pokeminList[i].height + ') ');
+if (pokeminList[i].height < 35  && pokeminList.height > 0) ;
+// Roselia's height 
+{document.write(' She is a small baddie.');
+}
+else if (pokeminList[i].height < 60 && pokeminList.height > 36 )
+{ document.write(' This buddy here is a midsize Pokemon');
+// Squrtile's height
+}
+else (pokemonList[i].height > 150)
+{ document.write (' Our friend here is human size; maddive isn\t it!? ' )
+// Slowbro's height
+}
+}
+
