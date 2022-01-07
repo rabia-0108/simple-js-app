9e966d5e8f36c075b83d7bea7d572424a1bd7f36
rabia-0108 <rabia.esya@gmail.com>
2022-01-07 17:14:55 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-  {  name:'Squirtle', type: ['water'], height: 50.8, pokedexNumber:'#07' };
+  { name:'Squirtle', type: ['water'], height: 50.8, pokedexNumber:'#07' },
   { name:'Slowbro', type:['water', 'psychic'], height: 152.5, pokedexNumber:'#80' },
   { name: 'Roselia', type: ['grass', 'poison'], height: 30.48, pokedexNumber:'#315'}
 ];
@@ -7,12 +7,12 @@ let pokemonList = [
 
 
 for (let i = 0; i < pokemonList.length; i++ ) {
-  document.write('<br>' + pokemonList[i].name + (' (height)') + pokeminList[i].height + ') ');
+  document.write('<br>' + pokemonList[i].name + (' (height)') + pokeminList[i].height  ) ;
 if (pokeminList[i].height < 35  && pokeminList.height > 0) ;
 // Roselia's height 
 {document.write(' She is a small baddie.');
 }
-else if (pokeminList[i].height < 60 && pokeminList.height > 36 )
+else if (pokeminList[i].height < 60 && pokeminList.height > 36 );
 { document.write(' This buddy here is a midsize Pokemon');
 // Squrtile's height
 }
